f7a3d9ac348bc9a34b004b142a8f68f4c1e7f5e0
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-10-31 18:51:05 +0200

    Create f_overloading01.cpp

---
+++ source_code/f_overloading01.cpp
@@ -0,0 +1,12 @@
+// f_overloading01.cpp
+//
+
+#include <iostream>
+
+int sum(int a, int b) { return a + b; }
+int sum(int a, int b, int c) { return a + b + c; }
+
+int main(){
+    // ...
+    return 0;
+}
