5a279f5128eb511c934601ac7300dd73fe8dfa36
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-10-31 19:45:38 +0200

    Update f_overloading02.cpp

--- source_code/f_overloading02.cpp
+++ source_code/f_overloading02.cpp
@@ -4,15 +4,11 @@
 #include <iostream>
 using namespace std;
 
-
 void print(const char* ch) { cout << ch; }
-
 void print(int a) { cout << a; }
 
-
 int main(){
-    const char *str = "codeMountain";
-    
+    const char *str = "codeMountain"; 
     int num = 15;
 
     print(str);
