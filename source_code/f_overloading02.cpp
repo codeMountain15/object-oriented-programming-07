6b3afe2bd63985bc169b8f98236b51519382f9dc
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-10-31 19:45:07 +0200

    Create f_overloading02.cpp

---
+++ source_code/f_overloading02.cpp
@@ -0,0 +1,22 @@
+// f_overloading02.cpp
+//
+
+#include <iostream>
+using namespace std;
+
+
+void print(const char* ch) { cout << ch; }
+
+void print(int a) { cout << a; }
+
+
+int main(){
+    const char *str = "codeMountain";
+    
+    int num = 15;
+
+    print(str);
+    print(num);
+
+    return 0;
+}
