5d2b5316564a80bd407835dcc3df032e7a04b2bd
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-11-07 22:53:44 +0200

    Update inheritance_multi.cpp

--- source_code/inheritance_multi.cpp
+++ source_code/inheritance_multi.cpp
@@ -8,18 +8,22 @@ class Mother
 {
 public:
 	Mother() { cout << "Mother's constructor activated\n"; }
+	~Mother() { cout << "Mother's destructor activated\n"; }
+
 };
 
 class Father
 {
 public:
 	Father() { cout << "Father's constructor activated\n"; }
+	~Father() { cout << "Father's destructor activated\n"; }
 };
 
 class Child : public Mother, public Father // Note the order!
 {
 public:
 	Child() { cout << "Child's constructor activated\n"; }
+	~Child() { cout << "Child's destructor activated\n"; }
 };
 
 int main()
